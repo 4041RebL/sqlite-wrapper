01b52229f620b5efb13d7478488bcbc9f23b5fb6
4041RebL <62462942+4041RebL@users.noreply.github.com>
2020-10-18 21:38:09 +0530

    updating version everytime is hel

--- reblite/__init__.py
+++ reblite/__init__.py
@@ -2,6 +2,6 @@ __title__ = 'FrickDB'
 __author__ = 'Harsh Vardhan'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2020 Harsh Vardhan'
-__version__ = '0.0.1'
+__version__ = '1.1.0'
 
 from .__main__ import *
